089f50d9390b3813d420a759df7d1f29371e2468
Juliana Nocchi Dobal <junocchi@gmail.com>
2025-03-14 17:34:13 +0000

    chore: remove v1

--- v1/src/envs.js
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-
-// to access local .env
-dotenv.config();
-export const ENV_VARS = {
-  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-};
